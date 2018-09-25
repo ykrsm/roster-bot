2b61a2ef18cc1f5e4f5e5e22b183962eef560634
ykrsm <yukikuro1993@gmail.com>
2018-09-24 21:25:54 -0500

    dockernized

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # roster-bot
+
+# Compile for linux
+```
+dep ensure
+env GOOS=linux GOARCH=386 go build -o main
+```
+# Start docker
+```
+docker-compose build
+docker-compose up
+```
--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
+	"github.com/bluele/slack"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	os.Exit(_main(os.Args[1:]))
-}
-
-func _main(args []string) int {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Listening slack event and response
-	fmt.Printf(os.Getenv("WEBHOOK_URL"))
+	hookURL := os.Getenv("WEBHOOK_URL")
+	hook := slack.NewWebHook(hookURL)
 
-	return 0
+	err2 := hook.PostMessage(&slack.WebHookPostPayload{
+
+		Text: "hello!",
+		Attachments: []*slack.Attachment{
+			{Text: "danger", Color: "danger"},
+		},
+	})
+	if err2 != nil {
+		panic(err)
+	}
 }
